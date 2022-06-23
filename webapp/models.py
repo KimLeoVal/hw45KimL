e637cc85d28d3d419072320a5a48a834d0c89714
KimLeoVal <kimleo@mail.ru>
2022-06-23 23:32:52 +0600

    create new model and create migration

--- webapp/models.py
+++ webapp/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class Article(models.Model):
+    description = models.TextField(max_length=3000, null=False, blank=False, verbose_name='Описание')
+    status = models.CharField(max_length=40, null=False, blank=False, default='new', verbose_name='Статус')
+    date = models.CharField(max_length=40, null=True, blank=True, verbose_name='Дата выполнения')
+
+    def __str__(self):
+        return f'id:{self.pk}, description: {self.description}'
