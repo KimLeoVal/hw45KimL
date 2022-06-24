14731695d4164df58047476d504038b9c885a550
KimLeoVal <kimleo@mail.ru>
2022-06-24 15:01:56 +0600

    create admin.site.register(Article)

--- webapp/urls.py
+++ webapp/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from webapp.views import index_view
+from webapp.views import index_view, create_article
 
 urlpatterns = [
     path('', index_view),
+    path('create/', create_article)
 
 ]
--- webapp/views.py
+++ webapp/views.py
@@ -1,6 +1,24 @@
 from django.shortcuts import render
 
+from webapp.models import Article
+
 
 def index_view(request):
-    context = {"test": 'TEST'}
+    articles = Article.objects.all()
+    context = {
+        'articles': articles
+    }
     return render(request, 'index.html', context)
+    return render(request, 'index.html', context)
+
+
+def create_article(request):
+    if request.method == "GET":
+        return render(request, "create.html")
+    else:
+        description = request.POST.get("title")
+        status = request.POST.get("author")
+        date = request.POST.get("content")
+        new_article = Article.objects.create(description=description, status=status, date=date)
+        context = {"article": new_article}
+        return render(request, "article_view.html", context)
