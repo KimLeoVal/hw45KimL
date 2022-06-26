4735c7b2b8a5a58502e354d5b02357505d186104
KimLeoVal <kimleo@mail.ru>
2022-06-27 01:04:29 +0600

    create brunch '46-1' and add new field 'title'

--- webapp/models.py
+++ webapp/models.py
@@ -4,7 +4,8 @@ STATUS_CHOICES = [('new', 'Новая'), ('in_progress', 'В процессе'),
 
 
 class Task(models.Model):
-    description = models.TextField(max_length=3000, null=False, blank=False, verbose_name='Описание')
+    title = models.TextField(max_length=30, default='', verbose_name='Задачи')
+    description = models.TextField(max_length=3000, null=True, blank=True, verbose_name='Описание')
     status = models.CharField(max_length=40, null=False, blank=False, choices=STATUS_CHOICES,
                               default=STATUS_CHOICES[0][0], verbose_name='Статус')
     date = models.CharField(max_length=40, null=True, blank=True, verbose_name='Дата выполнения')
